6bd0a68e141dcc5f93392885e69e4d44e852ae5c
Zerbu <zerbu@live.co.uk>
2025-12-05 20:31:21 +0000

    Fixed issue with the plugin not loading properly after opening a project

--- addons/scene_references/plugin.gd
+++ addons/scene_references/plugin.gd
@@ -5,16 +5,15 @@ var inspector: SceneInspector = SceneInspector.new()
 
 func _enable_plugin() -> void:
 	add_autoload_singleton("SceneReferenceManager", "scene_reference_manager.gd")
-	inspector.SceneReferenceManager = get_tree().get_root().get_node("/root/SceneReferenceManager")
 
 func _disable_plugin() -> void:
 	remove_autoload_singleton("SceneReferenceManager")
-	inspector.SceneReferenceManager = null
 
 
 func _enter_tree() -> void:
+	inspector.SceneReferenceManager = get_tree().get_root().get_node("/root/SceneReferenceManager")
 	add_inspector_plugin(inspector)
 
-
 func _exit_tree() -> void:
+	inspector.SceneReferenceManager = null
 	remove_inspector_plugin(inspector)
